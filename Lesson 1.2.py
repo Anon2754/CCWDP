7fadbb1707f56bfd0209193303182f4f575ff677
Anon2754 <raffsb92@gmail.com>
2025-04-16 12:38:57 -0500

    Add files via upload

---
+++ Lesson 1.2.py
@@ -0,0 +1,18 @@
+#what am I missing?
+name = "Chad Codemeister"
+print("Hello,", name)
+#Protect the muffins at all cost
+username = input("What is your name? ")
+print("Hello,", username)
+#The Cinnamon Streussel muffins are to be protected at all costs.
+gname = input("What is your gym name? ")
+favorite_color = input("What is your favorite color? ")
+print(gname, "'s favorite color is", favorite_color + ".")
+print("Not bad,", gname, "The color of champions is", favorite_color + ".")
+#The blueberry muffins are equal to Cinnamon Streussel muffins in terms of protection.
+name2 = input("What is your Street name? ")
+workout = input("What is your favorite workout? ")
+dream = input("What do you hope to achieve? ")
+print ("Nice to meet you,", name2 + ".")
+print ("A fan of ", workout + ", I see." , "\nBest of luck with", dream + "." )
+#The muffin kingdom remains safe.... for now.
---
+++ user_report.1.2.py
@@ -0,0 +1,6 @@
+#Guess the film, with a minor edit to the orignal quote
+name = input("What is your name?")
+favorite_food = input("What is your favorite food?")
+mastery = input("What would you like to master one day?")
+print("Subject:", name, "\nFavorite Food:", favorite_food, "\nFuture Mastery goal:", mastery)
+#Talking muffins, though popular in internet cuisine, are not often to be found in Washington DC
