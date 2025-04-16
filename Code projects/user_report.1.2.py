af9f8d7b48cbfc9ee6ee772930269324d952228c
Anon2754 <raffsb92@gmail.com>
2025-04-16 12:43:03 -0500

    Update user_report.1.2.py

--- Code projects/user_report.1.2.py
+++ Code projects/user_report.1.2.py
@@ -3,4 +3,6 @@ name = input("What is your name?")
 favorite_food = input("What is your favorite food?")
 mastery = input("What would you like to master one day?")
 print("Subject:", name, "\nFavorite Food:", favorite_food, "\nFuture Mastery goal:", mastery)
+
+print("Report complete, they are not to be underestimated.")
 #Talking muffins, though popular in internet cuisine, are not often to be found in Washington DC
